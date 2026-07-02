a3ebc5d9be58b37437943d2b78e3eef104df8083
Kriti98ranjan <kritiranjan196@gmail.com>
2026-07-02 19:53:12 +0530

    Navbar

--- app/components/ui/Navbar.jsx
+++ app/components/ui/Navbar.jsx
@@ -1,7 +1,9 @@
 export default function Navbar() {
   return (
     <header className="h-16 bg-[#283548] text-white flex items-center justify-between px-6">
-      <button className=" bg-blue-600 px-3 py-1.5 rounded hover:bg-amber-300">Menu</button>
+      <button className=" bg-blue-600 px-3 py-1.5 rounded hover:bg-amber-300">
+        Menu
+      </button>
 
       <div className="flex items-center gap-4">
         <input
@@ -9,9 +11,13 @@ export default function Navbar() {
           placeholder="Search for..."
           className="px-3 py-1.5 rounded text-sm text-black bg-white"
         />
-        <button className="bg-blue-600 px-3 py-1.5 rounded  hover:bg-amber-300">Search</button>
-        <button className="bg-blue-600 px-3 py-1.5 rounded  hover:bg-amber-300">User</button>
+        <button className="bg-blue-600 px-3 py-1.5 rounded  hover:bg-amber-300">
+          Search
+        </button>
+        <button className="bg-blue-600 px-3 py-1.5 rounded  hover:bg-amber-300">
+          User
+        </button>
       </div>
     </header>
-  )
+  );
 }
