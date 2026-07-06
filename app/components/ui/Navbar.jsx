d005f3ad19a6d5a268127d0f7b822dc6a7930f4a
Kriti98ranjan <kritiranjan196@gmail.com>
2026-07-06 16:46:14 +0530

    add html entity

--- app/components/ui/Navbar.jsx
+++ app/components/ui/Navbar.jsx
@@ -2,7 +2,7 @@ export default function Navbar() {
   return (
     <header className="h-16 bg-[#283548] text-white flex items-center justify-between px-6">
       <button className=" bg-blue-600 px-3 py-1.5 rounded hover:bg-amber-300">
-        Menu
+        &#9776;
       </button>
 
       <div className="flex items-center gap-4">
